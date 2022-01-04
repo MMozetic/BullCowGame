2c03d4c6e1eb39b2092fc08d31376a84bf3265a7
MMozetic <mozeticmilos97@gmail.com>
2022-01-04 10:56:20 +0100

    Reorder functions

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -1,6 +1,41 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "BullCowCartridge.h"
 
+void UBullCowCartridge::BeginPlay() // When the game starts
+{
+    Super::BeginPlay();
+    InitializeGame("1234");
+
+    // ****************************
+    // Introductory messages
+    // ****************************
+    PrintLine(TEXT("Hello my friend! Welcome to Bull Cows."));
+    PrintLine(TEXT("Guess the " + FString::FromInt(HiddenNumber.Len()) +
+        " digit number with all different digits."));
+    PrintLine(TEXT("Please press ENTER to continue..."));
+}
+
+void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
+{
+    ClearScreen();
+    if (GuessHiddenNumber(Input))
+    {
+        PrintLine(TEXT("You win!"));
+    }
+    else
+    {
+        if (Lives)
+        {
+            PrintLine(TEXT("Try again..."));
+            PrintLine(TEXT("Remaining Lives: " + FString::FromInt(Lives)));
+        }
+        else
+        {
+            PrintLine(TEXT("You lose!"));
+        }
+    }
+}
+
 bool UBullCowCartridge::AreDigitsDifferent(const FString& Number)
 {
     // Loop through the Number and check if some digits are repeating itself.
@@ -56,39 +91,4 @@ void UBullCowCartridge::InitializeGame(const FString& Number)
     // Set the new HiddenNumber and the number of Lives.
     HiddenNumber = Number;
     Lives = HiddenNumber.Len();
-}
-
-void UBullCowCartridge::BeginPlay() // When the game starts
-{
-    Super::BeginPlay();
-    InitializeGame("1234");
-
-    // ****************************
-    // Introductory messages
-    // ****************************
-    PrintLine(TEXT("Hello my friend! Welcome to Bull Cows."));
-    PrintLine(TEXT("Guess the " + FString::FromInt(HiddenNumber.Len()) + 
-        " digit number with all different digits."));
-    PrintLine(TEXT("Please press ENTER to continue..."));
-}
-
-void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
-{
-    ClearScreen();
-    if (GuessHiddenNumber(Input))
-    {
-        PrintLine(TEXT("You win!"));
-    }
-    else
-    {
-        if (Lives)
-        {
-            PrintLine(TEXT("Try again..."));
-            PrintLine(TEXT("Remaining Lives: " + FString::FromInt(Lives)));
-        }
-        else
-        {
-            PrintLine(TEXT("You lose!"));
-        }
-    }
 }
